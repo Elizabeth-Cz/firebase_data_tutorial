f4e1ff75e772dba611c627532fe34d3c9138b24a
Elizabeth Czarny <liz.cz91@gmail.com>
2022-11-20 17:41:51 +0100

    refactor[dashboard]: deleted accidental async import

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,7 +2,6 @@ import { auth } from "../utils/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { async } from "@firebase/util";
 
 export default function Dashboard() {
   const route = useRouter();
