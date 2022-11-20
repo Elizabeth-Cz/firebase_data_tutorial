082d731fc86348eb4a15b222fee71ad1dadf743c
Elizabeth Czarny <liz.cz91@gmail.com>
2022-11-20 14:12:25 +0100

    feat[login]: added redirection based on if user is logged in or not

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,20 +1,33 @@
 import { FcGoogle } from "react-icons/fc";
-import {
-  signInWithPopup,
-  GoogleAuthProvider,
-  GithubAuthProvider,
-} from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "../../utils/firebase";
+import { useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect } from "react";
 
 export default function Login() {
+  const route = useRouter();
+  const [user, loading] = useAuthState(auth);
+
   // sign in with google
 
   const googleProvider = new GoogleAuthProvider();
   const GoogleLogin = async () => {
     try {
       const result = await signInWithPopup(auth, googleProvider);
-    } catch (err) {}
+      route.push("/");
+    } catch (err) {
+      console.log(err);
+    }
   };
+
+  useEffect(() => {
+    if (user) {
+      route.push("/");
+    } else {
+      console.log("login");
+    }
+  }, [user]);
 
   return (
     <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
