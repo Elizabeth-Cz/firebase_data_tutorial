f794435b8178731ccacb14fce43b1399255b832d
Elizabeth Czarny <liz.cz91@gmail.com>
2022-11-20 17:43:01 +0100

    feat[index]: imported useEffect and useState

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,25 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import { db } from "../utils/firebase";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 
 export default function Home() {
+  //Create a state with all the posts
+  const [allPosts, setAllPosts] = useState([]);
+
+  const getPosts = async () => {
+    const collectionRef = collection(db, "posts");
+    const q = query(collectionRef, orderBy("timestamp", "desc"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setAllPosts(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    });
+    return unsubscribe;
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <div>
       <Head>
@@ -9,7 +28,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <div className="my-12 text-lg font-medium">
+        <h2>See what other people are saying</h2>
+      </div>
     </div>
   );
 }
