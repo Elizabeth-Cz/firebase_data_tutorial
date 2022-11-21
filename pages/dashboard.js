617973cb37699f2d1d58c1f7cd5fe17cb0fc7971
Elizabeth Czarny <liz.cz91@gmail.com>
2022-11-21 18:58:01 +0100

    feat[dashboard]: added delete and edit functionality

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,10 +2,18 @@ import { auth, db } from "../utils/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { collection, onSnapshot, query, where } from "firebase/firestore";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
 import Message from "../components/message";
 import { BsTrash2Fill } from "react-icons/bs";
 import { AiFillEdit } from "react-icons/ai";
+import Link from "next/link";
 
 export default function Dashboard() {
   const route = useRouter();
@@ -25,10 +33,11 @@ export default function Dashboard() {
     return unsubscribe;
   };
 
-  //delete post
-  // const deletePost = async (id) => {
-  //   const docRef =
-  // };
+  // delete post
+  const deletePost = async (id) => {
+    const docRef = doc(db, "posts", id);
+    await deleteDoc(docRef);
+  };
 
   //getUsers data
   useEffect(() => {
@@ -43,14 +52,19 @@ export default function Dashboard() {
           return (
             <Message {...post} key={post.id}>
               <div className="flex gap-4">
-                <button className="text-pink-600 flex items-center justify-center gap-2 py-2 text-sm">
+                <button
+                  onClick={() => deletePost(post.id)}
+                  className="text-pink-600 flex items-center justify-center gap-2 py-2 text-sm"
+                >
                   <BsTrash2Fill className=" text-2xl" />
                   Delete
                 </button>
-                <button className="text-teal-600 flex items-center justify-center gap-2 py-2">
-                  <AiFillEdit className=" text-2xl" />
-                  Edit
-                </button>
+                <Link href={{ pathname: "/post", query: post }}>
+                  <button className="text-teal-600 flex items-center justify-center gap-2 py-2">
+                    <AiFillEdit className=" text-2xl" />
+                    Edit
+                  </button>
+                </Link>
               </div>
             </Message>
           );
